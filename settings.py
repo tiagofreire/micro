6276fdcbad06cb7d6ef1bd561ae16e1ad26c0dfd
Tiago Freire <tiagowd@gmail.com>
2009-07-28 15:05:38 -0300

    add novo modulo

--- settings.py
+++ settings.py
@@ -15,8 +15,8 @@ MANAGERS = ADMINS
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'clanmetalceara'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'clanmetalceara'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'hostclanmetal'         # Not used with sqlite3.
-DATABASE_HOST = 'mysql.clanmetalceara.com'             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
